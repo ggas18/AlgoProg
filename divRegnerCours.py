96b62af0281f18617a661029d3f635c5222082f6
Arouna GANOU <arouna.ganou@gmail.com>
2017-10-14 18:21:30 +0200

    .

--- divRegnerCours.py
+++ divRegnerCours.py
@@ -4,7 +4,11 @@
 Created on Thu Oct  5 14:08:39 2017
 
 @author: arek
+
 """
+
+"""=============================Paramètres du fichier=================="""
+debug=False # pour debuguer le code notamment l'activation des fonctions prints
 """ Tri insertion"""
 def triInsertion(L):
     #entrée: liste L à trier
@@ -40,7 +44,7 @@ def triFusion(L):
 
 """ Exo 2 TD 2"""
 "Matrice A utilisée pour les tests de l'élement connexe"
-A=[13, -3, -25, 20, 3, 16, 23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
+A=[13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
 """force brute"""
 def sousListe(L,k):
     #
@@ -79,44 +83,116 @@ def sousMax(L,k):
 
 def sousTabConnexe(L):
     """
+    Cette fonction permet pour une liste L donnée de réels de trouver
+    le sous-tableau connexe de taille maximale.
+    ======================ENTREE=======================
+    L: une liste
+
+    =====================SORTIE========================
+    L_connexMaxi: le sous-tableau connexe de somme maximale
 
     """
     n=len(L)
+    """
+     on utilise un dictionnaire pour avoir un accès direct avec la somme
+     calculée comme clé et la sous-liste correspondante comme valeur du
+     associée
+    """
     dic_list=dict()
-    list_sum=[]
-    for k in range(n):
+    list_sum=[] # la liste des sommes
+    """
+    On recherche le sous-tableau de somme maximale de taille k et ceci pour
+    toutes les tailles k de 1 à n
+    """
+    for k in range(1,n+1):
+        """
+         s_k est liste dont le premier element est la somme et le deuxième
+         élément est la sous-liste correspondante.
+        """
         s_k=sousMax(L,k)
+        """
+        en enregistre s_k sous forme de cle:valeur avec cle la somme et valeur
+        la liste.
+        """
         dic_list[s_k[0]]=s_k[1]
+        """
+        on ajoute la somme actuelle à liste des sommes"""
         list_sum=list_sum+[s_k[0]]
-    return dic_list[max(list_sum)]
+    """
+    On cherche la somme maximale dans la liste de toute les sommes
+    """
+    somMax=max(list_sum)
+    """
+    On utilise ensuite le dictionnaire pour retrouver la liste correspondante à
+    cette somme somMax
+    """
+    L_connexMaxi=dic_list[somMax]
+    """
+    On retourne cette sous-liste: c'est la sous-liste connexe de somme maximale
+    """
+    return L_connexMaxi
 
 
 """diviser pour reigner"""
 def ssTabMil(A,deb,mil,fin):
-    somme=0
-    bas=mil
-    haut=mil
+    """
+    Cette fonction retourne le sous tableau maximal chevauchant
+    l'indice mil et contenu entre les indices deb et fin.
+    =========================ENTREE===============================
+    A: sous liste contenant tous les élements
+    deb: debut des indices possibles
+    mil: milieu sur lequel on se positionne
+    fin: fin des indices possibles
+    """
 
-    M=-inf
+    """
+    initialisation
+    """
+    somme=0
+    S=-inf # c'est la somme maximale par defaut pour
+    """
+    On cherche d'abord le sous tableau connexe de somme maximale et de plus
+    grand indice mil
+    """
     for i in range(mil,deb-1,-1):
         som_p=sum(A[i:mil])
-        if(M<som_p):
-            M=som_p
+        if(S<som_p):
+            S=som_p
             bas=i
 
-    M=-inf
+    S=-inf
     for i in range(mil,fin+1,1):
         som_p=sum(A[mil:i])
-        if(M<som_p):
-            M=som_p
+        if(S<som_p):
+            S=som_p
             haut=i
-    somme=sum(A[bas:haut+1])
+    somme=sum(A[bas:haut])
 
     return bas,haut,somme
 
-def ssTab(L):
+def ssTabMax(A,bas,haut):
+    """
+    """
+    mil=(haut+bas)//2
+    if debug:
+        print(bas, mil, haut)
 
-    return max()
+    if bas==mil:
+        return bas, haut, A[bas]
+    else:
+
+        som_bas=ssTabMax(A,bas,mil)
+        som_haut=ssTabMax(A,mil,haut)
+        som_mil=ssTabMil(A,bas,mil,haut)
+        if( som_bas[2]>som_haut[2] and som_bas[2]>som_mil[2]):
+            return ssTabMax(A,bas,mil)
+        elif ( som_haut[2]>som_bas[2] and som_haut[2]>som_mil[2]):
+            return ssTabMax(A,mil,haut)
+        else:
+            return ssTabMil(A,bas,mil,haut)
+
+def ssTabConnexMax(A):
+    return ssTabMax(A,0,len(A))
 
 
 
